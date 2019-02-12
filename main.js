58c54091c197a2d3f7af7ac8ffa05d4bb8c3307f
prezentace <prezentace@sspu-opava.cz>
2019-02-12 10:23:34 +0100

    Vytvořen main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function decToBin(num) {
+    var vysledek = '';
+    do {
+        vysledek = (num % 2) + vysledek;
+        num = Math.floor(num / 2);
+    } while(num != 0);
+    return vysledek;
+}
+
+console.log(decToBin(54));
+
